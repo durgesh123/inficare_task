2370f8a32911a338050ac3d502c5d77c3b7bec36
Durgesh Tripathi <durgesh.tripathi2@gmail.com>
2015-08-23 00:53:38 +0530

    changes in UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Fetch All User From User Table
+  def fetch_all_user
+    User.all.map(&:name)
+  end
+
+  #Fetch All Project From Project Table
+  def fetch_all_project
+    Project.all.map(&:name)
+  end
 end
